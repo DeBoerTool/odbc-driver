b6ff42a1c38b423ccdc9ab39292d1554d159073c
Dan Deboer <daniel.s.deboer@gmail.com>
2023-05-11 13:06:45 -0400

    new: add different grammar tests

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -1,7 +1,9 @@
-<?php
+<?php /** @noinspection PhpUnhandledExceptionInspection */
 
 namespace Dbt\Odbc\Tests;
 
+use Illuminate\Database\Query\Grammars\SqlServerGrammar as SqlServerQueryGrammar;
+use Illuminate\Database\Schema\Grammars\SqlServerGrammar as SqlServerSchemaGrammar;
 use Illuminate\Support\Facades\DB;
 
 class ConnectionTest extends TestCase
@@ -24,6 +26,35 @@ class ConnectionTest extends TestCase
 
         $this->assertNotNull(
             DB::connection('odbc2')->select(config('database.test_select')),
+        );
+    }
+
+    /** @test */
+    public function using_a_different_grammar(): void
+    {
+        config()->set('database.connections.odbc.grammar', [
+            'query' => SqlServerQueryGrammar::class,
+            'schema' => SqlServerSchemaGrammar::class,
+        ]);
+
+        $conn = DB::connection('odbc');
+
+        $this->assertInstanceOf(
+            SqlServerQueryGrammar::class,
+            $conn->getQueryGrammar(),
+        );
+
+        // The default schema grammar isn't set until the schema builder has
+        // been instantiated, which we do here by calling getSchemaBuilder().
+        $conn->getSchemaBuilder();
+
+        $this->assertInstanceOf(
+            SqlServerSchemaGrammar::class,
+            $conn->getSchemaGrammar(),
+        );
+
+        $this->assertNotNull(
+            DB::connection('odbc')->select(config('database.test_select')),
         );
     }
 }
